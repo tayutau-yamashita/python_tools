1187643fbc3e9508e310c27d1cd7c55f5995d100
山下拓也 <yamashita@tayutau-user-no-Mac-mini.local>
2016-05-18 13:34:00 +0900

    ちょっと改良。 ・実行から戻ってきた時にも表示 ・実行中は非表示に

--- SceneSelectEditorWindow.cs
+++ SceneSelectEditorWindow.cs
@@ -23,22 +23,32 @@ public class SceneSelectEditorWindow : EditorWindow {
 	void Initialize () {
 		Debug.Log ("---- SceneSelectEditorWindow:Initialize ----");
 		SceneNameList.Clear();
-		foreach (var guid in AssetDatabase.FindAssets("t:Scene")) {
-			var path = AssetDatabase.GUIDToAssetPath (guid);
-			if (path.IndexOf ("Test") != -1) {
-				continue;
-			}
-			SceneData data = new SceneData();
-			int index = path.LastIndexOf("/");
-			string name = path;
-			if (index != -1) {
-				name = path.Remove(0, index+1);
-			}
-			data.DisplaySceneName = name;
-			data.LoadScenePath = path;
-			SceneNameList.Add(data);
-//			Debug.Log(AssetDatabase.LoadMainAssetAtPath(path));
-		}	
+		CreateList();
+	}
+
+	void OnFocus() {
+		CreateList();
+	}
+
+	void CreateList() {
+		if (SceneNameList.Count == 0) {
+			foreach (var guid in AssetDatabase.FindAssets("t:Scene")) {
+				var path = AssetDatabase.GUIDToAssetPath (guid);
+				if (path.IndexOf ("Test") != -1) {
+					continue;
+				}
+				SceneData data = new SceneData();
+				int index = path.LastIndexOf("/");
+				string name = path;
+				if (index != -1) {
+					name = path.Remove(0, index+1);
+				}
+				data.DisplaySceneName = name;
+				data.LoadScenePath = path;
+				SceneNameList.Add(data);
+				//			Debug.Log(AssetDatabase.LoadMainAssetAtPath(path));
+			}	
+		}
 	}
 
 	// Update is called once per frame
@@ -47,21 +57,23 @@ public class SceneSelectEditorWindow : EditorWindow {
 	}
 
 	void OnGUI () {
-		EditorGUILayout.BeginVertical();
+		if (EditorApplication.isPlaying == false) {
+			EditorGUILayout.BeginVertical();
 
-		//PropertyFieldを使えば、型に合わせて基本通り描画してくれる。
-		//面倒なUndo処理とかも書かなくてもいい、
-//		EditorGUILayout.PropertyField(serializedObject.FindProperty("customSize"), new GUIContent("Size"));
-		ScrollPosition = EditorGUILayout.BeginScrollView(ScrollPosition, GUILayout.Height(200));
-		for (int i = 0; i < SceneNameList.Count; i++) {
-			if (GUILayout.Button(SceneNameList[i].DisplaySceneName, GUILayout.Width(200f))) {
-				if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) {
-					EditorSceneManager.OpenScene(SceneNameList[i].LoadScenePath);
+			//PropertyFieldを使えば、型に合わせて基本通り描画してくれる。
+			//面倒なUndo処理とかも書かなくてもいい、
+			//		EditorGUILayout.PropertyField(serializedObject.FindProperty("customSize"), new GUIContent("Size"));
+			ScrollPosition = EditorGUILayout.BeginScrollView(ScrollPosition, GUILayout.Height(200));
+			for (int i = 0; i < SceneNameList.Count; i++) {
+				if (GUILayout.Button(SceneNameList[i].DisplaySceneName, GUILayout.Width(200f))) {
+					if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) {
+						EditorSceneManager.OpenScene(SceneNameList[i].LoadScenePath);
+					}
 				}
 			}
-		}
 
-		EditorGUILayout.EndScrollView();
-		EditorGUILayout.EndVertical();
+			EditorGUILayout.EndScrollView();
+			EditorGUILayout.EndVertical();
+		}
 	}
 }
